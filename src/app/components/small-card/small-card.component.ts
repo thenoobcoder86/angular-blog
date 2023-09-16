1e3aed6e3091e6edfbcb055888f6a02a986093d2
Akieu <andreoliveira886@yahoo.com.br>
2023-09-16 20:51:38 -0300

    content created

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -12,4 +12,7 @@ export class SmallCardComponent {
 
   @Input()
   cardTitle: string = ""
+
+  @Input()
+  Id: string = "0"
 }
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id":"1",        
+        "title": "Nova animação dos Cavaleiros do Zodíaco pela Hakuren Studios",
+        "description": "Nova animação feita por um estúdio independente do México já se encontra com o primeiro episódio disponível para delírio dos fãs.",
+        "photoCover": "https://naradia.com.br/wp-content/uploads/2023/08/hakuren.png"
+    },
+    {
+        "id":"2",       
+        "title": "The Marvels vem aí",
+        "description": "Novo filme do MCU já causa hype nos cinemas e redes sociais com seu novo trailer.",
+        "photoCover": "https://wegotthiscovered.com/wp-content/uploads/2023/09/the-marvels-imax-poster-1.jpg?w=1200"
+    },
+    {
+        "id":"3",       
+        "title": "Spider-man 2 para PS5 tem novo trailer divulgado",
+        "description": "Novo trailer do jogo vem bombando no Youtube com vários elogios e promete muita ação e aventura.",
+        "photoCover": "https://meups.com.br/wp-content/uploads/2023/07/Marvels-Spider-Man-2-Peter-e-Miles.jpg"
+    },
+    {
+        "id":"4",       
+        "title": "Aquaman 2: trailer surpreende",
+        "description": "Trailer do filme deixa os geeks ansiosos, será que vai ser isso tudo mesmo?",
+        "photoCover": "https://uploads.folhabv.com.br/2023/09/Fim-epico-do-DCEU-Aquaman-2-ganha-sinopse-incrivel-jpg.webp"
+    }
+]
